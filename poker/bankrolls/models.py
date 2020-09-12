5b677e0c96c313dbf5b9bfa1e4fdee6e62917e7b
Harrison Stein <hmstein@MacBook-Pro.local>
2020-09-12 18:04:40 -0400

    added Session and resent migrations

--- poker/bankrolls/models.py
+++ poker/bankrolls/models.py
@@ -1,6 +1,10 @@
-from django.db import models
+#f
+#from django.db import models
+from djongo import models
+from django.contrib.auth.models import User
 
 class Bankroll(models.Model):
+    #user = models.OneToOneField(User, on_delete=models.CASCADE)
     balance = models.DecimalField(default=0, decimal_places=2, max_digits=50)
     starting_balance = models.DecimalField(default=0, decimal_places=2, max_digits=50)
     created_dttm = models.DateField('date created')
@@ -17,4 +21,17 @@ class Transaction(models.Model):
     #def __str__(self):
     #    return self.transaction_text
 
-#class Session(models.Model):
+class Session(models.Model):
+    bankroll = models.ForeignKey(Bankroll, on_delete=models.CASCADE)
+    start_dttm = models.DateTimeField()
+    end_dttm = models.DateTimeField()
+    buy_in = models.DecimalField(default=0, decimal_places=2, max_digits=50)
+    cash_out = models.DecimalField(default=0, decimal_places=2, max_digits=50)
+    notes = models.TextField(max_length=500, blank=True)
+    max_table_size = models.IntegerField(default=0)
+    transactions = models.ArrayReferenceField(
+        to=Transaction,
+        on_delete=models.CASCADE,
+    )
+    session_type = models.CharField(max_length=32)
+    
