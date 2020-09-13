b768d9c076f27905a638b904d86af95e0bbbaa0d
Harrison Stein <hmstein@MacBook-Pro.local>
2020-09-13 10:34:12 -0400

    register Session

--- poker/bankrolls/admin.py
+++ poker/bankrolls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Bankroll, Transaction
+from .models import Bankroll, Transaction, Session
 
 admin.site.register(Bankroll)
 admin.site.register(Transaction)
+admin.site.register(Session)
